a48e240b006a9007d5ae5fbfb4f074814c9e8fc0
MecaVol <landry@mecavol.com>
2020-05-10 18:19:11 +0400

    Geolocator dependency added

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class HomePage extends StatelessWidget {
 
@@ -11,7 +12,40 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text(this.title),
       ),
-      body: null,
+      body: LocationWidget(),
+    );
+  }
+}
+
+class LocationWidget extends StatefulWidget {
+  _LocationWidgetState createState() => _LocationWidgetState();
+}
+
+class _LocationWidgetState extends State<LocationWidget> {
+
+  Position _position;
+
+  void _updatePosition() async {
+    final Geolocator geolocator = Geolocator()..forceAndroidLocationManager;
+
+    _position = await geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.best);
+    setState(() {
+      //print('pos!');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Text("LAT: ${_position?.latitude ?? "XX"}, LNG: ${_position?.longitude ?? "XX"}"),
+        FlatButton(
+          child: Text('GET LOCATION'),
+          onPressed: () {
+            _updatePosition();
+          },
+        )
+      ],
     );
   }
 }
